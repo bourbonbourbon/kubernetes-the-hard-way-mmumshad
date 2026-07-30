80903d37994f157f13bea36ba21e36127dcd23ee
Abdul Rahman <109749723+bourbonbourbon@users.noreply.github.com>
2026-07-31 03:23:52 +0530

    Changed Guest Additions version to 7.2.14

--- vagrant/ubuntu/vagrant/install-guest-additions.sh
+++ vagrant/ubuntu/vagrant/install-guest-additions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-GUEST_ADDITION_VERSION=5.2.4
+GUEST_ADDITION_VERSION=7.2.14
 GUEST_ADDITION_ISO=VBoxGuestAdditions_${GUEST_ADDITION_VERSION}.iso
 GUEST_ADDITION_MOUNT=/media/VBoxGuestAdditions
 
-apt-get install linux-headers-$(uname -r) build-essential dkms
+apt-get install "linux-headers-$(uname -r)" build-essential dkms
 
 wget http://download.virtualbox.org/virtualbox/${GUEST_ADDITION_VERSION}/${GUEST_ADDITION_ISO}
 mkdir -p ${GUEST_ADDITION_MOUNT}
