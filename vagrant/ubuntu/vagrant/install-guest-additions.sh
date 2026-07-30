bffc7df78606b4bd6521e3fb0d682c8cae98d19e
Abdul Rahman <109749723+bourbonbourbon@users.noreply.github.com>
2026-07-31 03:42:51 +0530

    Script changes for install-guest-additions.sh

--- vagrant/ubuntu/vagrant/install-guest-additions.sh
+++ vagrant/ubuntu/vagrant/install-guest-additions.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+set -e
+
 GUEST_ADDITION_VERSION=7.2.14
 GUEST_ADDITION_ISO=VBoxGuestAdditions_${GUEST_ADDITION_VERSION}.iso
 GUEST_ADDITION_MOUNT=/media/VBoxGuestAdditions
 
-apt-get install "linux-headers-$(uname -r)" build-essential dkms
+apt-get update
 
-wget http://download.virtualbox.org/virtualbox/${GUEST_ADDITION_VERSION}/${GUEST_ADDITION_ISO}
+DEBIAN_FRONTEND=noninteractive apt-get install "linux-headers-$(uname -r)" build-essential dkms wget
+
+wget https://download.virtualbox.org/virtualbox/${GUEST_ADDITION_VERSION}/${GUEST_ADDITION_ISO}
 mkdir -p ${GUEST_ADDITION_MOUNT}
 mount -o loop,ro ${GUEST_ADDITION_ISO} ${GUEST_ADDITION_MOUNT}
 sh ${GUEST_ADDITION_MOUNT}/VBoxLinuxAdditions.run
